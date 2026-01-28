4072e6c69aa1801d0f2a235556a40653fef3f6b3
Oskar <Oskarwidell@outlook.com>
2026-01-28 14:33:08 +0100

    Meged the DBcontext and built a magration from the merged DBcontext.

--- Datalagring-Oskar-Videll.Infrastructure/Data/DeltagareDBContext.cs
+++ Datalagring-Oskar-Videll.Infrastructure/Data/DeltagareDBContext.cs
@@ -8,6 +8,12 @@ public sealed class DeltagareDBContext(DbContextOptions<DeltagareDBContext> opti
     public DbSet<DeltagareEntity> Deltagare => Set<DeltagareEntity>();
     public DbSet<Role_Entity> Roles => Set<Role_Entity>();
     public DbSet<StatusTypeEntity> StatusTypes => Set<StatusTypeEntity>();
+    public DbSet<Ort_Entity> Ort => Set<Ort_Entity>();
+    public DbSet<Kurstillfalle_Entity> KursTillfalle => Set<Kurstillfalle_Entity>();
+    public DbSet<Kurs_Entity> Kurs => Set<Kurs_Entity>();
+    public DbSet<KursRegi_Entity> KursRegi => Set<KursRegi_Entity>();
+    public DbSet<KurstillfalleLarare_Entity> Larare_Kurstillfalle => Set<KurstillfalleLarare_Entity>();
+    public DbSet<Larare_Entity> Larare => Set<Larare_Entity>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -99,5 +105,126 @@ public sealed class DeltagareDBContext(DbContextOptions<DeltagareDBContext> opti
 
         });
 
+        modelBuilder.Entity<Larare_Entity>(entity =>
+        {
+            entity.ToTable("Larare");
+            entity.HasKey(e => e.LarareEmail).HasName("PK_Larare_LarareEmail");
+            entity.Property(e => e.LarareEmail)
+                .IsRequired()
+                .HasMaxLength(255);
+            entity.Property(e => e.fornamn)
+                .IsRequired()
+                .HasMaxLength(100);
+            entity.Property(e => e.mellannamn)
+                .HasMaxLength(100);
+            entity.Property(e => e.efternamn)
+                .IsRequired()
+                .HasMaxLength(100);
+            entity.Property(e => e.kompentens)
+                .IsRequired()
+                .HasMaxLength(500);
+            entity.HasIndex(e => e.LarareEmail, "UQ_Larare_LarareEmail").IsUnique();
+            entity.ToTable(tb => tb.HasCheckConstraint("CK_Larare_LarareEmail_NotEmpty", "LTRIM(RTRIM('LarareEmail')) <> ''"));
+        });
+
+        modelBuilder.Entity<Kurstillfalle_Entity>(entity =>
+        {
+            entity.ToTable("KursTillfalle");
+            entity.HasKey(e => e.KurstillfalleId).HasName("PK_KursTillfalle_KursId");
+            entity.Property(e => e.KurstillfalleId);
+            entity.Property(e => e.KursKod)
+                .IsRequired()
+                .HasMaxLength(50);
+            entity.Property(e => e.Ortid)
+                .IsRequired();
+            entity.Property(e => e.MaxSeats)
+                .IsRequired();
+            entity.Property(e => e.Startdatum)
+                .IsRequired();
+            entity.Property(e => e.Slutdatum)
+                .IsRequired();
+
+            entity.HasOne<Kurs_Entity>()
+                .WithMany()
+                .HasForeignKey(e => e.KursKod)
+                .OnDelete(DeleteBehavior.Restrict)
+                .HasConstraintName("FK_KursTillfalle_Kurs_Kurskod");
+
+            entity.HasOne<Ort_Entity>()
+                .WithMany()
+                .HasForeignKey(e => e.Ortid)
+                .OnDelete(DeleteBehavior.Restrict)
+                .HasConstraintName("FK_KursTillfalle_Ort_Ortid");
+
+        });
+
+        modelBuilder.Entity<Kurs_Entity>(entity =>
+        {
+            entity.ToTable("Kurs");
+            entity.HasKey(e => e.Kurskod).HasName("PK_Kurs_Kurskod");
+            entity.Property(e => e.Kurskod)
+                .IsRequired()
+                .HasMaxLength(50);
+            entity.Property(e => e.Kursnamn)
+                .IsRequired()
+                .HasMaxLength(200);
+            entity.Property(e => e.Beskrivning)
+                .IsRequired()
+                .HasMaxLength(1000);
+            entity.HasIndex(e => e.Kursnamn, "UQ_Kurs_Kursnamn").IsUnique();
+        });
+
+        modelBuilder.Entity<Ort_Entity>(entity =>
+        {
+            entity.ToTable("Ort");
+            entity.HasKey(e => e.OrtId).HasName("PK_Ort_Ortid");
+            entity.Property(e => e.OrtNamn)
+                .IsRequired()
+                .HasMaxLength(100);
+            entity.HasIndex(e => e.OrtNamn, "UQ_Ort_Ortnamn").IsUnique();
+        });
+
+        modelBuilder.Entity<KursRegi_Entity>(entity =>
+        {
+            entity.ToTable("KursRegi");
+            entity.HasKey(e => new { e.KurstillfalleId, e.DeltagareEmail }).HasName("PK_KursRegi_KurstillfalleId");
+            entity.Property(e => e.KurstillfalleId)
+                .IsRequired();
+            entity.Property(e => e.DeltagareEmail)
+                .IsRequired()
+                .HasMaxLength(255);
+            entity.Property(e => e.RegiDatum)
+                .IsRequired();
+            entity.Property(e => e.status)
+                .IsRequired()
+                .HasMaxLength(50);
+
+        });
+
+        modelBuilder.Entity<KurstillfalleLarare_Entity>(entity =>
+        {
+            entity.ToTable("KurstillfalleLarare");
+            entity.HasKey(e => new { e.KurstillfalleId, e.LarareEmail }).HasName("PK_KurstillfalleLarare_KurstillfalleId_LarareEmail");
+            entity.Property(e => e.KurstillfalleId)
+                .IsRequired();
+            entity.Property(e => e.LarareEmail)
+                .IsRequired()
+                .HasMaxLength(255);
+        });
+
+        modelBuilder.Entity<Kurstillfalle_Entity>()
+            .HasMany(k => k.KursRegi)
+            .WithOne()
+            .HasForeignKey(kr => kr.KurstillfalleId)
+            .OnDelete(DeleteBehavior.Cascade)
+            .HasConstraintName("FK_KursRegi_KursTillfalle_KurstillfalleId");
+
+        modelBuilder.Entity<Kurstillfalle_Entity>()
+            .HasMany(k => k.KurstillfalleLarare)
+            .WithOne()
+            .HasForeignKey(kl => kl.KurstillfalleId)
+            .OnDelete(DeleteBehavior.Cascade)
+            .HasConstraintName("FK_KurstillfalleLarare_KursTillfalle_KurstillfalleId");
     }
+
 }
